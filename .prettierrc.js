734b7eb00fa44bc47f568b7f40aacc3ee5544dec
Kirill Kudryavcev <rtutiui@mail.ru>
2022-10-07 14:49:59 +0300

    Добавлена возможность устанавливать ключ вручную. Добавлена проверка на ошибку и корректность ключа.

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+// https://prettier.io/docs/en/options.html
+
+module.exports = {
+	arrowParens: 'avoid', // Include parentheses around a sole arrow function parameter
+	printWidth: 200, // Specify the line length that the printer will wrap on
+	semi: false, // Print semicolons at the ends of statements
+	singleQuote: true, // Use single quotes instead of double quotes
+	useTabs: false, // Indent lines with tabs instead of spaces
+}
--- src/components/Encrypter.vue
+++ src/components/Encrypter.vue
@@ -1,16 +1,9 @@
 <template>
   <form name="encform" onsubmit="return false;">
     <div class="main">
-      <label for="exampleFormControlTextarea1"
-             class="form-label">
-        Исходные данные</label>
-      <textarea name="box1"
-                v-model="box1"
-                class="form-control"
-                id="exampleFormControlTextarea1"
-                placeholder="Введите текст для зашифровки/расшифровки..."
-                rows="3"></textarea>
-
+      <label for="exampleFormControlTextarea1" class="form-label"> Исходные данные</label>
+      <textarea name="box1" v-model="box1" class="form-control" id="exampleFormControlTextarea1" placeholder="Введите текст для зашифровки/расшифровки..." rows="3"></textarea>
+      <b-form-textarea name="key" v-model="key" class="key" id="textarea-small" size="sm" placeholder="Введите ключ..."></b-form-textarea>
       <!-- <div class="salts">
         <button class="btn btn-primary"
                 @click="sha256()">
@@ -26,239 +19,316 @@
         </button>
       </div> -->
       <div class="cryptButtons">
-        <button class="btn btn-primary"
-                @click="VernamCrypt()">
-          Шифр Вернама
-        </button>
+        <button class="btn btn-primary" @click="VernamCrypt()">Шифр Вернама</button>
 
-        <button class="btn btn-primary"
-                @click="CezarCrypt()">
-          Шифр Цезаря
-        </button>
-        <button class="btn btn-primary"
-                @click="RC4Crypt()">
-          RC4
-        </button>
+        <button class="btn btn-primary" @click="CezarCrypt()">Шифр Цезаря</button>
+        <button class="btn btn-primary" @click="RC4Crypt()">RC4</button>
       </div>
-
 
       <div class="cryptButtonsMain">
-        <button class="btn btn-primary"
-                @click="AesCrypt()">
-          AES
-        </button>
-        <button class="btn btn-primary"
-                @click="DesCrypt()">
-          DES
-        </button>
-        <button class="btn btn-primary"
-                @click="TripleDESCrypt()">
-          Triple DES
-        </button>
-        <button class="btn btn-primary"
-                @click="RabbitCrypt()">
-          Rabbit
-        </button>
+        <button class="btn btn-primary" @click="AesCrypt()">AES</button>
+        <button class="btn btn-primary" @click="DesCrypt()">DES</button>
+        <button class="btn btn-primary" @click="TripleDESCrypt()">Triple DES</button>
+        <button class="btn btn-primary" @click="RabbitCrypt()">Rabbit</button>
       </div>
 
-
-      <label for="exampleFormControlTextarea2"
-             class="form-label">
-        Результат программы</label>
-      <textarea name="box2"
-                class="form-control"
-                v-model="box2"
-                id="exampleFormControlTextarea2"
-                rows="3"
-                placeholder="Данные после шифрации/дешифрации"></textarea>
+      <label for="exampleFormControlTextarea2" class="form-label"> Результат программы</label>
+      <textarea name="box2" class="form-control" v-model="box2" id="exampleFormControlTextarea2" rows="3" placeholder="Данные после шифрации/дешифрации"></textarea>
 
       <div class="unCryptButtons1">
-        <button class="btn btn-primary"
-                @click="CezarunUncrypt()">
-          Расшифровать шифр Цезаря
-        </button>
-        <button class="btn btn-primary"
-                @click="AesUncrypt()">
-          Расшифровать AES
-        </button>
-        <button class="btn btn-primary"
-                @click="DesUncrypt()">
-          Расшифровать DES
-        </button>
+        <button class="btn btn-primary" @click="CezarunUncrypt()">Расшифровать шифр Цезаря</button>
+        <button class="btn btn-primary" @click="AesUncrypt()">Расшифровать AES</button>
+        <button class="btn btn-primary" @click="DesUncrypt()">Расшифровать DES</button>
       </div>
 
       <div class="unCryptButtons2">
-        <button class="btn btn-primary"
-                @click="TripleDESUncrypt()">
-          Расшифровать Triple DES
-        </button>
-        <button class="btn btn-primary"
-                @click="RabbitUncrypt()">
-          Расшифровать Rabbit
-        </button>
-        <button class="btn btn-primary"
-                @click="RC4Uncrypt()">
-          Расшифровать RC4
-        </button>
+        <button class="btn btn-primary" @click="TripleDESUncrypt()">Расшифровать Triple DES</button>
+        <button class="btn btn-primary" @click="RabbitUncrypt()">Расшифровать Rabbit</button>
+        <button class="btn btn-primary" @click="RC4Uncrypt()">Расшифровать RC4</button>
       </div>
-
-
     </div>
   </form>
 </template>
 
 <script>
-var CryptoJS = require("crypto-js");
+var CryptoJS = require('crypto-js')
 
 export default {
   name: 'EncrypterFunctions',
   data() {
     return {
       box1: null,
-      box2: null
+      box2: null,
+      key: null,
     }
   },
   props: {
-    msg: String
+    msg: String,
   },
   methods: {
-
     sha256() {
-      this.box2 = CryptoJS.SHA256(this.box1);
+      this.box2 = CryptoJS.SHA256(this.box1)
     },
 
     md5() {
-      this.box2 = CryptoJS.MD5(this.box1);
+      this.box2 = CryptoJS.MD5(this.box1)
     },
 
     ripemd() {
-      this.box2 = CryptoJS.RIPEMD160(this.box1);
+      this.box2 = CryptoJS.RIPEMD160(this.box1)
     },
-
-
 
     CezarCrypt() {
-         let theText = this.box1
-         let output  = new String;
-         let Temp    = new Array();
-         let Temp2   = new Array();
-         let TextSize = theText.length;
-        for (let i = 0; i < TextSize; i++) {
-          let rnd = Math.round(Math.random() * 122) + 68;
-          Temp[i] = theText.charCodeAt(i) + rnd;
-          Temp2[i] = rnd;
-        }
-        for (let i = 0; i < TextSize; i++) {
-          output += String.fromCharCode(Temp[i], Temp2[i]);
-        }
-        this.box2 = output;
-      },
-
-    VernamCrypt() {
-         var theText = this.box1
-        // генератор случайных чисел в заданном диапазоне
-        function getRandomInt(min, max) {
-          return Math.floor(Math.random() * (max - min)) + min;
-        }
-        var input, output, key;
-        var inp, k;
-        input = theText
-
-        key = prompt("Введите ключ и запишите его - он потребуется для расшифровки сообщения");
-
-        // если длина ключа меньше длины сообщения — говорим пользователю и генерируем свой ключ
-        if ((key.length) < (input.length)) {
-          alert("Ключ короче сообщения, это небезопасно. Скопируйте новый сгенерированный ключ из консоли браузера.");
-          key = "";
-          // генерируем новый ключ такой же длины, как и сообщение
-          for (var i = 0; i < input.length; i++) {
-            key += String.fromCharCode(getRandomInt(0, 66535));
-          }
-          console.log("Скопируйте новый ключ ↓");
-          console.log(key);
-        }
-        output = "";
-        for (i = 0; i < input.length; i++) {
-          inp = input.charCodeAt(i);
-          k = key.charCodeAt(i);
-          output += String.fromCharCode(inp ^ k);
-        }
-        this.box2 = output;
-      },
-
-    RC4Crypt() {
-      this.box2 = CryptoJS.RC4.encrypt(this.box1, "Secret Passphrase").toString();
+      let theText = this.box1
+      let output = new String()
+      let Temp = new Array()
+      let Temp2 = new Array()
+      let TextSize = theText.length
+      for (let i = 0; i < TextSize; i++) {
+        let rnd = Math.round(Math.random() * 122) + 68
+        Temp[i] = theText.charCodeAt(i) + rnd
+        Temp2[i] = rnd
+      }
+      for (let i = 0; i < TextSize; i++) {
+        output += String.fromCharCode(Temp[i], Temp2[i])
+      }
+      this.box2 = output
     },
 
+    VernamCrypt() {
+      var theText = this.box1
+      // генератор случайных чисел в заданном диапазоне
+      function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min)) + min
+      }
+      var input, output, key
+      var inp, k
+      input = theText
 
+      key = this.key //prompt('Введите ключ и запишите его - он потребуется для расшифровки сообщения')
+
+      // если длина ключа меньше длины сообщения — говорим пользователю и генерируем свой ключ
+      if (key.length < input.length) {
+        alert('Ключ короче сообщения, это небезопасно. Скопируйте новый сгенерированный ключ из консоли браузера.')
+        key = ''
+        // генерируем новый ключ такой же длины, как и сообщение
+        for (var i = 0; i < input.length; i++) {
+          key += String.fromCharCode(getRandomInt(0, 66535))
+        }
+        console.log('Скопируйте новый ключ ↓')
+        //console.log(key)
+        this.key = key
+      }
+      output = ''
+      for (i = 0; i < input.length; i++) {
+        inp = input.charCodeAt(i)
+        k = key.charCodeAt(i)
+        output += String.fromCharCode(inp ^ k)
+      }
+      this.box2 = output
+    },
+
+    RC4Crypt() {
+      try {
+        this.box2 = CryptoJS.RC4.encrypt(this.box1, this.key).toString()
+      } catch (e) {
+        this.$bvModal.msgBoxOk(e.message, {
+          title: 'Ошибка',
+          size: 'sm',
+          buttonSize: 'sm',
+          okVariant: 'danger',
+          headerClass: 'p-2 border-bottom-0',
+          footerClass: 'p-2 border-top-0',
+          headerBgVariant: 'danger',
+          centered: true,
+        })
+      }
+    },
 
     AesCrypt() {
-        this.box2 = CryptoJS.AES.encrypt(this.box1, 'secret key').toString()
-      },
+      try {
+        this.box2 = CryptoJS.AES.encrypt(this.box1, this.key).toString()
+      } catch (e) {
+        this.$bvModal.msgBoxOk(e.message, {
+          title: 'Ошибка',
+          size: 'sm',
+          buttonSize: 'sm',
+          okVariant: 'danger',
+          headerClass: 'p-2 border-bottom-0',
+          footerClass: 'p-2 border-top-0',
+          headerBgVariant: 'danger',
+          centered: true,
+        })
+      }
+    },
 
     DesCrypt() {
-         this.box2 = CryptoJS.DES.encrypt(this.box1, 'secret key').toString();
-      },
+      try {
+        this.box2 = CryptoJS.DES.encrypt(this.box1, this.key).toString()
+      } catch (e) {
+        this.$bvModal.msgBoxOk(e.message, {
+          title: 'Ошибка',
+          size: 'sm',
+          buttonSize: 'sm',
+          okVariant: 'danger',
+          headerClass: 'p-2 border-bottom-0',
+          footerClass: 'p-2 border-top-0',
+          headerBgVariant: 'danger',
+          centered: true,
+        })
+      }
+    },
 
     TripleDESCrypt() {
-        this.box2 = CryptoJS.TripleDES.encrypt(this.box1, "Secret Passphrase").toString();
-      },
+      try {
+        this.box2 = CryptoJS.TripleDES.encrypt(this.box1, this.key).toString()
+      } catch (e) {
+        this.$bvModal.msgBoxOk(e.message, {
+          title: 'Ошибка',
+          size: 'sm',
+          buttonSize: 'sm',
+          okVariant: 'danger',
+          headerClass: 'p-2 border-bottom-0',
+          footerClass: 'p-2 border-top-0',
+          headerBgVariant: 'danger',
+          centered: true,
+        })
+      }
+    },
 
     RabbitCrypt() {
-        this.box2 = CryptoJS.Rabbit.encrypt(this.box1, "secret key").toString();
-      },
-
-
+      try {
+        this.box2 = CryptoJS.Rabbit.encrypt(this.box1, this.key).toString()
+      } catch (e) {
+        this.$bvModal.msgBoxOk(e.message, {
+          title: 'Ошибка',
+          size: 'sm',
+          buttonSize: 'sm',
+          okVariant: 'danger',
+          headerClass: 'p-2 border-bottom-0',
+          footerClass: 'p-2 border-top-0',
+          headerBgVariant: 'danger',
+          centered: true,
+        })
+      }
+    },
 
     CezarunUncrypt() {
-        let theText = this.box2
-        let output  = new String;
-        let Temp    = new Array();
-        let Temp2   = new Array();
-        let TextSize = theText.length;
-        for (let i = 0; i < TextSize; i++) {
-          Temp[i] = theText.charCodeAt(i);
-          Temp2[i] = theText.charCodeAt(i + 1);
-        }
-        for (let i = 0; i < TextSize; i = i+2) {
-          output += String.fromCharCode(Temp[i] - Temp2[i]);
-        }
-        this.box2 = output;
-      },
+      let theText = this.box2
+      let output = new String()
+      let Temp = new Array()
+      let Temp2 = new Array()
+      let TextSize = theText.length
+      for (let i = 0; i < TextSize; i++) {
+        Temp[i] = theText.charCodeAt(i)
+        Temp2[i] = theText.charCodeAt(i + 1)
+      }
+      for (let i = 0; i < TextSize; i = i + 2) {
+        output += String.fromCharCode(Temp[i] - Temp2[i])
+      }
+      this.box2 = output
+    },
 
     DesUncrypt() {
-        var bytes = CryptoJS.DES.decrypt(this.box2, 'secret key');
-        this.box2 = bytes.toString(CryptoJS.enc.Utf8);
-      },
+      try {
+        var bytes = CryptoJS.DES.decrypt(this.box2, this.key)
+        this.box2 = bytes.toString(CryptoJS.enc.Utf8)
+      } catch (e) {
+        this.$bvModal.msgBoxOk(e.message, {
+          title: 'Ошибка',
+          size: 'sm',
+          buttonSize: 'sm',
+          okVariant: 'danger',
+          headerClass: 'p-2 border-bottom-0',
+          footerClass: 'p-2 border-top-0',
+          headerBgVariant: 'danger',
+          centered: true,
+        })
+      }
+    },
 
     AesUncrypt() {
-        var bytes  = CryptoJS.AES.decrypt(this.box2, 'secret key');
-        this.box2 = bytes.toString(CryptoJS.enc.Utf8);
-      },
+      try {
+        var bytes = CryptoJS.AES.decrypt(this.box2, this.key)
+        this.box2 = bytes.toString(CryptoJS.enc.Utf8)
+      } catch (e) {
+        this.$bvModal.msgBoxOk(e.message, {
+          title: 'Ошибка',
+          size: 'sm',
+          buttonSize: 'sm',
+          okVariant: 'danger',
+          headerClass: 'p-2 border-bottom-0',
+          footerClass: 'p-2 border-top-0',
+          headerBgVariant: 'danger',
+          centered: true,
+        })
+      }
+    },
 
     TripleDESUncrypt() {
-      var bytes = CryptoJS.TripleDES.decrypt(this.box2, "Secret Passphrase");
-      this.box2 = bytes.toString(CryptoJS.enc.Utf8)
+      try {
+        var bytes = CryptoJS.TripleDES.decrypt(this.box2, this.key)
+        this.box2 = bytes.toString(CryptoJS.enc.Utf8)
+      } catch (e) {
+        this.$bvModal.msgBoxOk(e.message, {
+          title: 'Ошибка',
+          size: 'sm',
+          buttonSize: 'sm',
+          okVariant: 'danger',
+          headerClass: 'p-2 border-bottom-0',
+          footerClass: 'p-2 border-top-0',
+          headerBgVariant: 'danger',
+          centered: true,
+        })
+      }
     },
 
     RabbitUncrypt() {
-      var bytes = CryptoJS.Rabbit.decrypt(this.box2, "secret key")
-      this.box2 = bytes.toString(CryptoJS.enc.Utf8)
+      try {
+        var bytes = CryptoJS.Rabbit.decrypt(this.box2, this.key)
+        this.box2 = bytes.toString(CryptoJS.enc.Utf8)
+      } catch (e) {
+        this.$bvModal.msgBoxOk(e.message, {
+          title: 'Ошибка',
+          size: 'sm',
+          buttonSize: 'sm',
+          okVariant: 'danger',
+          headerClass: 'p-2 border-bottom-0',
+          footerClass: 'p-2 border-top-0',
+          headerBgVariant: 'danger',
+          centered: true,
+        })
+      }
     },
 
     RC4Uncrypt() {
-      var bytes = CryptoJS.RC4.decrypt(this.box2, "Secret Passphrase")
-      this.box2 = bytes.toString(CryptoJS.enc.Utf8)
-    }
-
-  }
+      try {
+        var bytes = CryptoJS.RC4.decrypt(this.box2, this.key)
+        this.box2 = bytes.toString(CryptoJS.enc.Utf8)
+      } catch (e) {
+        this.$bvModal.msgBoxOk(e.message, {
+          title: 'Ошибка',
+          size: 'sm',
+          buttonSize: 'sm',
+          okVariant: 'danger',
+          headerClass: 'p-2 border-bottom-0',
+          footerClass: 'p-2 border-top-0',
+          headerBgVariant: 'danger',
+          centered: true,
+        })
+      }
+    },
+  },
 }
 </script>
 <style scoped lang="scss">
-
 .cryptButtons {
   box-sizing: border-box;
   padding: 10px;
+}
+
+.key {
+  margin-top: 20px;
 }
 
 .salts {
@@ -284,5 +354,4 @@ button {
   padding-top: 10px;
   padding-bottom: 10px;
 }
-
 </style>
